3c359318cc93d29dab36d5aa723f6bbe8cc8385d
Ionut Velicu <velicu.ionut@gmail.com>
2022-03-16 19:03:02 +0200

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -57,7 +57,7 @@ const Home: NextPage = () => {
           <Service name={"Design Editor"} status={Normal}/>
           <Service name={"Link Pages"} status={Normal}/>
           <Service name={"Online Menus"} status={Normal}/>
-          <Service name={"QR Codes"} status={Outage}/>
+          <Service name={"QR Codes"} status={Normal}/>
         </div>
       </main>
 
